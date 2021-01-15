43b2f5586596e79311fec4bddfc702fb676a4144
Liroto <ololoshin228@gmail.com>
2021-01-16 01:50:46 +0300

    Сам себе выдумал задание. Разобрался с классами, конструкторами, агрегацией и композицией, а так же с спецификаторами public и private.

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,20 +1,155 @@
-﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <string>
+using namespace std;
 
-#include <iostream>
+
+class Engine //при помощи данного класса будет демонстрироваться агрегация
+{
+private:
+	string engineType;
+	int horsePowers;
+
+public:
+	Engine()
+	{
+		//пустой конструктор,чтобы компилятор не ругался
+	}
+
+	//работа сетеорв и гетеров
+
+	void SetEngineType(string orderedEngineType)
+	{
+		engineType = orderedEngineType;
+	}
+
+	string GetEngineType()
+	{
+		return engineType;
+	}
+	
+
+	void SetHorsePowers(int orderedHorsePowers)
+	{
+		horsePowers = orderedHorsePowers;
+	}
+
+	int GetHorsePowers()
+	{
+		return horsePowers;
+	}
+
+
+
+	//работа конструктора
+
+	Engine(string orderedEngineType, int orderedHorsePowers) 
+	{
+		engineType = orderedEngineType;
+		horsePowers = orderedHorsePowers;
+	}
+
+	//вывод заказанного двигателя
+
+	void OrderedEngine() 
+	{
+		cout << "Тип двигателя: " + engineType + "\nМощность: " + to_string(horsePowers) + " л.с." << endl; 
+	}
+};
+
+class Auto 
+{
+private:
+	string autoBrand, bodyNumber;
+	int mileage;
+	bool roadAccident;
+	Engine currentEngine;
+	
+
+
+	string Crash() //функция, строго определяющая была ли машина в аварии
+	{
+		if (roadAccident) return "да";
+		else return "нет";
+	}
+
+
+
+	class Wheels  //при помощи данного класса будет демонстрироваться композиция
+	{
+	private:
+		string wheelBrand;
+		int wheelSize;
+
+	public:
+		Wheels(string orderedBrand, int orderedSize)
+		{
+			wheelBrand = orderedBrand;
+			wheelSize = orderedSize;
+		}
+		
+		
+		string GetWheelBrand() 
+		{
+			return wheelBrand;
+		}
+
+		int GetWheelSize() 
+		{
+			return wheelSize;
+		}
+
+		void WheelsReview() 
+		{
+			cout << "Колёса от: " << wheelBrand << "\nДиаметр: " << to_string(wheelSize) << " дюймов" << endl;
+		}
+
+	};
+
+	Wheels currentWheels = Wheels("Spin Tiers", 24);
+	//Wheels currentWheels("BBS", 17) не понимаю,почему такая запсь не работает
+
+public:
+	Auto(string orderedBrand, string currentBodyNumber,bool wasInAccident, int currentMileage, Engine orderedEngine)
+	{
+		autoBrand = orderedBrand;
+		bodyNumber = currentBodyNumber;
+		mileage = currentMileage;
+		roadAccident = wasInAccident;
+		currentEngine = orderedEngine;
+	}
+
+
+	void AutoReview() 
+	{
+		cout << "Марка авто: " << autoBrand << "\nНомер кузова: " << bodyNumber << "\nПробег: " << to_string(mileage) << " км\nБыла в ДТП: " << Crash() << endl;
+		cout << "Тип двигателя: " << currentEngine.GetEngineType() << "\nМощность двигателя: " << to_string(currentEngine.GetHorsePowers()) << " л.с." << endl;
+		currentWheels.WheelsReview();
+	}
+};
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	setlocale(LC_ALL, "Russian");
+	
+
+	Engine diesel; //пустой конструктор не имеет параметров и не ругается
+	diesel.SetEngineType("Diesel");
+	diesel.SetHorsePowers(450);
+
+	Engine gas("Gas engine", 180); //переопределённый/перегруженный конструктор принял параметры и тоже не ругается
+	
+	
+	Auto NewAuto("BMW", "EX-234-AA-56781-BW", false, 187000, gas); //примеры объектов класса с использованием агрегации и композиции
+	NewAuto.AutoReview();
+
+	cout << endl << endl;
+	cout << endl << endl;
+
+	Auto AnotherAuto("Kenworth", "ST-777-BD-90345", true, 345100, diesel);
+	AnotherAuto.AutoReview();
+	
+
+
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
